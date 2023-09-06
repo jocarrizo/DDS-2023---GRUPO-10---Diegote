ad673545031c566e7f96b62617315b6d8b91fa69
Joaquin Santiago Carrizo <jocarrizo@frba.utn.edu.ar>
2023-09-06 08:37:08 -0300

    Elimino avance en auth0, TODO

--- Entrega 3/src/main/java/Control/Registrador.java
+++ Entrega 3/src/main/java/Control/Registrador.java
@@ -6,20 +6,11 @@ import java.util.Scanner;
 
 public class Registrador
 {
-    static Scanner sc = new Scanner(System.in);
-
-    public static void registrar(String user, String pass)
+    public static boolean registrar(String user, String pass)
     {
+        if (user.equals(pass) || !validadorDePass.validar(pass)) return false;
 
-        while( user.equals(pass) || !validadorDePass.validar(pass) )
-        {
-        System.out.println("Ingrese nueva contraseña: ");
-        pass = sc.nextLine();
-        }
-
-          Usuario new_user = new Usuario(user,pass);
-          System.out.println("Nuevo usuario \nid: "+new_user.getId_usuario()+"\npass: "+ new_user.getContrasenia());
-
-//          guardar usuario en Base.
+        Usuario new_user = new Usuario(user,pass);
+        return true;
     }
 }
