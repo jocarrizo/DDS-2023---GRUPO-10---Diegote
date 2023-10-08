39b8fb0b9776fbfa2ccf87f10c1bcb9331815c07
Joaquin Santiago Carrizo <jocarrizo@frba.utn.edu.ar>
2023-10-07 21:26:51 -0300

    Cambios post entrega 4

---
+++ Entrega 4/src/main/java/Usuarios/Horario.java
@@ -0,0 +1,23 @@
+package Usuarios;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class Horario {
+    @Id
+    @GeneratedValue
+    private long id_horario;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_PERFIL")
+    private Perfil perfil_asoc;
+
+    @Column(name = "HORARIO")
+    private Date horario;
+}
